9095651d4f8a214239cf652c059a807d972c5e92
Infant Ryan <108726059+INFANT-RYAN@users.noreply.github.com>
2025-06-02 00:14:06 +0530

    Add files via upload
    PYTHON SCRIPT

---
+++ autoCharge.py
@@ -0,0 +1,20 @@
+from  pyfirmata import Arduino
+import psutil
+import time
+
+board = Arduino('COM5')
+led_pin = board.get_pin("d:7:o")
+
+while True:
+    battery = psutil.sensors_battery()
+    print(battery.percent)
+    if battery.percent < 20:
+        led_pin.write(0)
+        print("on")
+    elif battery.percent >80:
+        led_pin.write(1)
+        print("on")
+    else:
+        led_pin.write(1)
+        print("enough percentage")
+    time.sleep(10)
